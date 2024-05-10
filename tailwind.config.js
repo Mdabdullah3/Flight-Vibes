37bab92fa782197f50610e932030406989c13c1f
Mdabdullah3 <mdabdullahnoman777@gmail.com>
2024-05-10 09:22:59 +0600

    latest changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  daisyui: {
+    themes: [
+      {
+        light: {
+          primary: "#E74C3C",
+          secondary: "#141618",
+          neutral: "#ffff",
+          "base-100": "#ffff",
+          info: "#ECF0F3",
+          success: "#ffff",
+          warning: "#DF7E07",
+          error: "#FA5C5C",
+          lightPrimary: "#F4F7FE",
+          blueSecondary: "#4318FF",
+          brandLinear: "#868CFF",
+        },
+      },
 
+    ],
+  },
+  plugins: [require("daisyui")],
+}
